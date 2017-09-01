8e8d5e2ef539f8fab09955134a6070ccb1eed43a
Fabian fabian.soosaithasan@rwth-aachen.de <fabian.soosaithasan@rwth-aachen.de>
2017-09-01 13:19:21 +0200

    Create a so called dump component

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// simple function to return an array of books
+export default function() {
+  return [
+    { title: 'JS: The good parts' },
+    { title: 'Harry Potter' },
+    { title: 'The Darknet' },
+    { title: 'Ruby On Rails' }
+  ]
+}
